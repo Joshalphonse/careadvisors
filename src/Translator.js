d625f1431a5cae19d79a0bd2e92d63210e21be2d
Joshua Alphonse <joshalphonse@gmail.com>
2019-10-23 20:52:49 -0400

    gorboyoyo translation out of jsx

--- src/Translator.js
+++ src/Translator.js
@@ -1,101 +1,93 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import './Translator.css'
+import React, { Component } from "react";
+import axios from "axios";
+import "./Translator.css";
 class Translator extends Component {
-	constructor(props) {
-		super(props)
+  constructor(props) {
+    super(props);
 
-		this.state = {
-      submittedTexts:'',
-	  translations:[],
-	  output:''
-	  
-		}
-	}
+    this.state = {
+      submittedTexts: "",
+      translations: [],
+      output: ""
+    };
+  }
 
-	changeHandler = e => {
-		this.setState({ [e.target.name]: e.target.value })
-	}
+  changeHandler = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-	submitHandler = e => {
-		e.preventDefault()
-		console.log(this.state)
-		axios
-      .post('https://72exx40653.execute-api.us-east-1.amazonaws.com/prod/translateEnglishToAlien', {
-        textToTranslate: this.state.submittedTexts
-  })
-			.then(response => {
-        this.setState({translations: response.data})
-        
-        console.log(response.data)
-        
-			})
-			.catch(error => {
-				console.log(error)
+  submitHandler = e => {
+    e.preventDefault();
+    console.log(this.state);
+    axios
+      .post(
+        "https://72exx40653.execute-api.us-east-1.amazonaws.com/prod/translateEnglishToAlien",
+        {
+          textToTranslate: this.state.submittedTexts
+        }
+      )
+      .then(response => {
+        this.setState({
+          translations: response.data
+            .map(s => {
+              let splitDorb = s.split(/([A-Za-z])/);
+              let sum = parseInt(splitDorb[0]) + parseInt(splitDorb[2]);
+              return splitDorb[1] + "yo" + sum;
+            })
+            .join("")
+        });
+
+        console.log(response.data);
       })
-      
-	}
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
+  verify = e => {
+    e.preventDefault();
+    console.log(this.state);
+    axios
+      .post(
+        "https://72exx40653.execute-api.us-east-1.amazonaws.com/prod/confirmtranslation",
+        {
+          textToTranslate: this.state.submittedTexts
+        }
+      )
+      .then(response => {
+        this.setState({ translations: response.data });
 
-	verify = e => {
-		e.preventDefault()
-		console.log(this.state)
-		axios
-      .post('https://72exx40653.execute-api.us-east-1.amazonaws.com/prod/confirmtranslation', {
-        textToTranslate: this.state.submittedTexts
-  })
-			.then(response => {
-        this.setState({translations: response.data})
-        
-        console.log(response.data)
-        
-			})
-			.catch(error => {
-				console.log(error)
+        console.log(response.data);
       })
-      
-	}
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
-	processText= inputText => {
+  processText = inputText => {};
 
-	}
+  render() {
+    const { submittedTexts } = this.state;
 
-	render() {
-    const { submittedTexts } = this.state
-    
-		return (
-			<div>
-				<form onSubmit={this.submitHandler}>
-					<div>
-						<input
-							type="text"
-							name="submittedTexts"
-							value={submittedTexts.toLowerCase()}
-							onChange={this.changeHandler}
-												/>
-					</div>
-					
-					<button type="submit">Submit</button>
-				</form>
-        
-        <p>
-        { this.state.translations.map(s => {
-    let splitDorb = s.split(/([A-Za-z])/)
-    let sum = parseInt(splitDorb[0]) + parseInt(splitDorb[2])
-    return splitDorb[1] + 'yo' + sum
-  }).join('')
-  
-}
-      </p>
-      
- 
-        
-			</div>
-		)
-	}
+    return (
+      <div>
+        <form onSubmit={this.submitHandler}>
+          <div>
+            <input
+              type="text"
+              name="submittedTexts"
+              value={submittedTexts.toLowerCase()}
+              onChange={this.changeHandler}
+            />
+          </div>
+
+          <button type="submit">Submit</button>
+        </form>
+
+        <p>{this.state.translations}</p>
+      </div>
+    );
+  }
 }
 
-export default Translator
-
-
-
+export default Translator;
